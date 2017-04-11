78d5f07d03b047ed1ce0da5f69e51d094f83bf65
caperez <caperez@blizzard.com>
2017-04-10 20:58:20 -0700

    started to added images using jquery

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var charSlots = [10, 5, 3, 7];
+var charSlot1 = $("#charSlot1");
+var charSlot2 = $("#charSlot2");
+var charSlot3 = $("#charSlot3");
+var charSlot4 = $("#charSlot4");
+
+
+charSlot1 = $("<img>");
+charSlot1.addClass("char-image");
+charSlot1.attr("src", "assets/images/girl.jpg");
+charSlot1.attr("height")
+charSlot1.attr("char-value", charSlots[i]);
+$("#charSlot1").append(charSlot1);
