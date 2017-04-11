f3999d73ecf4275f4e271d1429e5e5647122d16f
caperez <caperez@blizzard.com>
2017-04-10 21:49:13 -0700

    added 4 characters to the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,11 @@
-var charSlots = [10, 5, 3, 7];
-var charSlot1 = $("#charSlot1");
-var charSlot2 = $("#charSlot2");
-var charSlot3 = $("#charSlot3");
-var charSlot4 = $("#charSlot4");
+$(document).ready(function(){
+	var charSlot1 = $("#charSlot1");
+	var charSlot2 = $("#charSlot2");
+	var charSlot3 = $("#charSlot3");
+	var charSlot4 = $("#charSlot4");
 
-
-charSlot1 = $("<img>");
-charSlot1.addClass("char-image");
-charSlot1.attr("src", "assets/images/girl.jpg");
-charSlot1.attr("height")
-charSlot1.attr("char-value", charSlots[i]);
-$("#charSlot1").append(charSlot1);
+	charSlot1.append("<img src='assets/images/girl.jpg' height='100px' />");
+	charSlot2.append("<img src='assets/images/donnieYen.jpg' height='100px' />");
+	charSlot3.append("<img src='assets/images/robot.jpg' height='100px' />");
+	charSlot4.append("<img src='assets/images/guy.jpg' height='100px' />");
+})
